0e58aef61ba0d116e04cb62518f5a2e10f976244
Silvia Benitez Masip <silviam92@gmail.com>
2025-07-05 13:34:21 +0200

    commit 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,27 @@
-'use strict';
+"use strict";
 
-console.log('>> Somos Equipo 5');
+console.log(">> Somos Equipo 5");
 
-document.querySelector('h1').innerHTML = "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.";
+document.querySelector("h1").innerHTML =
+  "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.";
 
-document.querySelector('.p1').innerHTML = "Hola mundo";
-
+document.querySelector(".p1").innerHTML = "Hola mundo";
 
 document.querySelector(".ada2").style.display = "none";
 
+document.querySelector(".ex4").innerHTML = "<h2>Lorem ipsum</h2>";
+//  "<img src="http://via.placeholder.com/350x150">;
 
-document.querySelector(".ex4").innerHTML =
+const button = document.querySelector(".button-add");
 
-     "<h2>Lorem ipsum</h2>";
-    //  "<img src="http://via.placeholder.com/350x150">;
+button.style.border = "none";
+
+button.addEventListener("click", () => {
+  console.log("Has hehco click");
+  const originalText = document.querySelector(".p2 p").textContent;
+
+  const nuevoParrafo = document.createElement("p");
+  nuevoParrafo.textContent = originalText;
+
+  document.body.appendChild(nuevoParrafo);
+});
