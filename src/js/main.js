334f61fc4ccb18029422e3e4be0e4506641f70e5
Rosana-Moya <rosanamoya88@gmail.com>
2025-07-05 11:00:59 +0200

    ejercicio 4

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('>> Ready :)');
+console.log('>> Somos Equipo 5');
+
+document.querySelector('h1').innerHTML = "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.";
+
+document.querySelector('.p1').innerHTML = "Hola mundo";
+
+
+document.querySelector(".ada2").style.display = "none";
+
+
+document.querySelector(".ex4").innerHTML =
+
+     "<h2>Lorem ipsum</h2>";
+    //  "<img src="http://via.placeholder.com/350x150">;
